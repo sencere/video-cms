267d19a7c0debc8c8d5a814b9a9195dd3e9fa9e0
sencere <morpheusspirit003@gmail.com>
2021-10-08 21:30:05 +0200

    fix database issues

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
+// use Laravel\Scout\Searchable;
 
 class Channel extends Model
 {
-    use Searchable;
+    // use Searchable;
 
     protected $fillable = [
         'name',
--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -24,7 +24,7 @@
                 </div>
                 <button type="submit" class="btn btn-default">Search</button>
             </form>
-            
+
             <ul class="nav navbar-nav">
                 &nbsp;
             </ul>
